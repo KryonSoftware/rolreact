133631c6bae12ffc6bbf24ba190c6129a39c1a89
KryonSoftware <jomipagu94@gmail.com>
2020-09-19 20:41:09 +0200

    Updates

---
+++ precache-manifest.51d44c5f0082fd09627f11e7b217ff42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fec5f519fd9f48c44493834a297654",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f0ff5bb1989be8fb94e",
+    "url": "/portfolio/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "bc2bb8eefb408891e586",
+    "url": "/portfolio/static/css/main.94e9e964.chunk.css"
+  },
+  {
+    "revision": "4f0ff5bb1989be8fb94e",
+    "url": "/portfolio/static/js/2.7a12f033.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/portfolio/static/js/2.7a12f033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2bb8eefb408891e586",
+    "url": "/portfolio/static/js/main.c6608901.chunk.js"
+  },
+  {
+    "revision": "b7a54034a6e8395e86e7",
+    "url": "/portfolio/static/js/runtime-main.616b009b.js"
+  },
+  {
+    "revision": "79d4a75ebbc92af2b2e97cf8fcc23ae3",
+    "url": "/portfolio/static/media/kumo.79d4a75e.jpg"
+  },
+  {
+    "revision": "97cb14f4ad744e5ce1eea8251ce96490",
+    "url": "/portfolio/static/media/rick.97cb14f4.jpg"
+  }
+]);
